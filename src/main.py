93f88594b26eb7a01ee2bebac02618fbd5f5940d
Evvive <evvive@evvive.local>
2022-01-30 17:18:32 +0100

    Fixed linter errors and other

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from sys import argv
-from logger import Logger, LoggerError, ErrType, StatusType
-from lexer import Lexer
+from call.logger import Logger, LoggerError, ErrType, StatusType
+from call.lexer import Lexer
+
 
 def main(argv) -> int:
-    argc     = len(argv)
-    log      = True
+    argc = len(argv)
+    log = True
     filename = ""
 
     if argc == 2:
@@ -28,14 +29,16 @@ def main(argv) -> int:
         return 1
 
     code = ""
-    f    = None
+    f = None
 
     try:
         logger.print(ErrType.INFO, "Reading file...", StatusType.NORMAL)
         f = open(filename, "r")
         code = f.read()
     except FileNotFoundError:
-        if filename == "": filename = "nothing"
+        if filename == "":
+            filename = "nothing"
+
         logger.print(ErrType.FATAL, f"Given file ({filename}) is invalid", StatusType.EXIT)
 
     finally:
@@ -44,16 +47,18 @@ def main(argv) -> int:
         else:
             return 1
 
-        if code == "": return 1
+        if code == "":
+            return 1
 
         logger.print(ErrType.INFO, "Starting Lexer...", StatusType.NORMAL)
         lexer = Lexer(code)
 
         lexer.lex()
 
-        logger.print(ErrType.INFO, f"Ending process with status 0", StatusType.EXIT)
+        logger.print(ErrType.INFO, "Ending process with status 0", StatusType.EXIT)
 
         return 0
+
 
 if __name__ == "__main__":
     exit_code = main(argv)
