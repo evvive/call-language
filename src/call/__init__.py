ba6241563e1a53bba746da8d2755a0b1d91bea38
Evvive <evvive@evvive.local>
2022-01-30 16:59:26 +0100

    Reorganized project

---
+++ src/call/__init__.py
@@ -0,0 +1,7 @@
+from lexer import Lexer
+
+
+class Call:
+    """CALL Main class"""
+    def __init__(self, filename: str):
+        self.filename = filename
--- src/colors.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, unique
-
-@unique
-class Colors(Enum):
-    RED   = "\033[31m",
-    WHITE = "\033[0m",
-    GREEN = "\033[32m",
-    BLUE  = "\033[34m",
-    PINK  = "\033[35m"
