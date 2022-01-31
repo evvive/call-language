a72d4eea2f3ca2ee0b78c5ad6d89b6d0d8ddb35c
Johannes Darienberg <jdarienberg@gmail.com>
2022-01-31 11:00:47 +0100

    Testing other types

--- src/test.py
+++ src/test.py
@@ -13,6 +13,7 @@ from call.logger import Logger, ErrType, StatusType
 # branch = BranchGenerator(lexer.tokens)
 # branch.generate()
 
+# BOOLEAN TEST
 logger = Logger(__name__, True)
 logger.print(ErrType.INFO, "logger started", StatusType.BEGIN)
 
@@ -24,9 +25,19 @@ except TypeGenerationError:
     logger.print(ErrType.INFO, "(bool2) test passed", StatusType.NORMAL)
     pass
 
+# STRING TEST
 s = TypeGenerator(Types.STR, "Flex Mark")
 logger.print(ErrType.INFO, "(str) test passed", StatusType.NORMAL)
 
-logger.print(ErrType.INFO, "all test passed!", StatusType.EXIT)
+# F64 TEST
+f = TypeGenerator(Types.F64, "0.646464646464")
+logger.print(ErrType.INFO, "(f64) test passed", StatusType.NORMAL)
 
+try:
+    f = TypeGenerator(Types.F64, "0.64.64.64.64.64.")
+except TypeGenerationError:
+    logger.print(ErrType.INFO, "(f64_2) test passed", StatusType.NORMAL)
+    pass
+
+logger.print(ErrType.INFO, "all test passed!", StatusType.EXIT)
 exit(0)
