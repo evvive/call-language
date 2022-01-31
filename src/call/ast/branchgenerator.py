c318207c904b760d03cad775884384f14996579c
Johannes Darienberg <jdarienberg@gmail.com>
2022-01-31 10:27:21 +0100

    Improved branch generator

--- src/call/ast/branchgenerator.py
+++ src/call/ast/branchgenerator.py
@@ -1,5 +1,9 @@
 from call.lexer.tokens import Token, Tokens
 from call.ast.types import Branch, Instructions
+from call.ast.errors import BranchError
+from call.types import TypeGenerator
+from call.types.errors import TypeGenerationError
+from call.types.types import Types
 
 
 class BranchGenerator:
@@ -13,29 +17,52 @@ class BranchGenerator:
 
         return
 
-    def _handle_keyword(self, token: Token) -> None:
-        if token.value.strip() in Instructions:
-            print("Recognized instruction")
-            return
-
-    """
-    Handles every token
-    """
-
-    def _handle_token(self, token: Token) -> None:
-        print("Token:", token.token, "\nValue:", token.value)
-
-        if token.token == Tokens.KEYWORD:
-            print("Keyword!")
-            self._handle_keyword(token)
-
-        return
-
-    """
-    Generates the branch
-    """
-
     def generate(self) -> None:
+        """
+        Generates the branch
+        """
+
         print("Generating AST...")
-        for token in self.tokens:
-            self._handle_token(token)
+        self.instruction: Instructions
+
+        if len(self.tokens) == 0:
+            raise BranchError(self.tokens, self.tokens[0], "Empty line")
+
+        if self.tokens[0].token != Tokens.KEYWORD:
+            raise BranchError(
+                    self.tokens,
+                    self.tokens[0],
+                    "1st token should be ALWAYS a keyword"
+                    )
+
+        instruction = self.tokens[0].value.lower().strip()
+
+        if instruction not in Instructions:
+            raise BranchError(
+                    self.tokens,
+                    self.tokens[0],
+                    "Invalid instruction"
+                    )
+
+        if instruction == "mpush":
+            print("MPUSH")
+            if len(self.tokens) != 2:
+                raise BranchError(
+                        self.tokens,
+                        self.tokens[0],
+                        "mpush has invalid args"
+                        )
+
+            param = self.tokens[1]
+
+            if param.token == Tokens.NUMBER:
+                param_type = Types.F64
+            elif param.token == Tokens.STRING:
+                param_type = Types.STR
+            elif param.token == Tokens.KEYWORD:
+                if param.value.strip().lower() == "true" or param.value.strip().lower() == "false":
+                    param_type = Types.BOL
+
+            var = TypeGenerator(param_type, param.value)
+
+            print(var)
