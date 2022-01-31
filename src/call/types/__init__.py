c440bff6c068beca2c862ccec36dda333301ce2e
Johannes Darienberg <jdarienberg@gmail.com>
2022-01-31 10:27:04 +0100

    Added TypeGenerator

--- src/call/types/__init__.py
+++ src/call/types/__init__.py
@@ -3,18 +3,31 @@ from call.types.errors import TypeGenerationError
 
 
 class TypeGenerator:
-    def __init__(self, type: Types, value: Value) -> None:
+    def __init__(self, type: Types, value: str) -> None:
+        self.type = type
+        self.value: Value = None
+
         if type == Types.STR:
-            value = str(value)
+            self.value = Value(Types.STR, value)
         elif type == Types.F64:
-            self._f64()
+            self.value = self.f64(value)
+        else:
+            raise TypeGenerationError(type, value, "Unknown type")
 
         return
 
-    def _f64(self):
+    @staticmethod
+    def f64(value: str):
         try:
-            f64 = float(self.value.value)
+            f64 = float(value)
         except ValueError:
-            raise TypeGenerationError(self.type, self.value, "Invalid f64")
-        finally:
-            return f64
+            raise TypeGenerationError(Types.F64, value, "Invalid f64")
+
+        return Value(Types.F64, f64)
+
+    @staticmethod
+    def bol(value: str) -> Value:
+        if value.value.strip().lower() == "true":
+            return Value(Types.BOL, True)
+        elif value.strip().lower() == "false":
+            return Value(Types.BOL, False)
