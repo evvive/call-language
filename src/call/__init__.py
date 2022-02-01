cf86fed269646aa8fe363a02a0056908d10cfcf3
Johannes Darienberg <jdarienberg@gmail.com>
2022-02-01 11:05:35 +0100

    Moving to StackBasic repo

--- src/call/__init__.py
+++
@@ -1,7 +0,0 @@
-from call.lexer import Lexer
-
-
-class Call:
-    """CALL Main class"""
-    def __init__(self, filename: str):
-        self.filename = filename
--- src/call/ast/__init__.py
+++
@@ -1,9 +0,0 @@
-from call.lexer.tokens import Token
-from call.ast.types import Branch
-
-
-# TODO: Do this later, first do ASTLine
-class ASTGenerator:
-    def __init__(self, tokens: list) -> None:
-        self.tokens: list[Token]
-        self.ast: list[Branch]
